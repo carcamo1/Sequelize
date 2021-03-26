ac73aaa93035b8d4572052c4d93bf60243728147
carcamo1 <carcamo1@users.noreply.github.com>
2021-03-26 19:57:59 -0400

    update

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ import apiRoutes from './routes/apiRoutes.js';
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+const staticFolder = 'public';
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(staticFolder));
 
-app.use('/api', apiRoutes);
+app.use('/api', apiRoutes); 
 
 async function bootServer() {
   try {
@@ -24,3 +26,4 @@ async function bootServer() {
 }
 
 bootServer();
+
